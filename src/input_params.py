Fixed the normalization constant calculation for M12 method. had missed this after updateding the DTD to (t/1Gyr)^s

src/input_params.py
@@ -145,8 +145,8 @@ class Input_Parameters(object):
         self.Z = '0.0190'
         self.fbhb = 0.0
         self.t_cutoff = 1.e9 * u.yr
-        self.slopes = np.arange(-3., 0.0001, 0.01)
-        #self.slopes = np.arange(-3., 0.0001, 0.05) #Coarse for testing.
+        #self.slopes = np.arange(-3., 0.0001, 0.01)
+        self.slopes = np.arange(-3., 0.0001, 0.05) #Coarse for testing.
         #self.slopes = np.arange(-3., 0.0001, 0.1) #Coarse for testing.
         #self.slopes = np.arange(-3., 0.0001, 0.5) #Coarse for testing.
         self.slopes[abs(self.slopes + 1.) < 1.e-6] = -0.9999